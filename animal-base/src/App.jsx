0c33b39ed3de863324f54184b13f1e120b338ee4
Hannah2609 <hann7419@stud.kea.dk>
2023-11-02 08:33:38 +0100

    sort

--- animal-base/src/App.jsx
+++ animal-base/src/App.jsx
@@ -1,6 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import { useState } from "react";
 import { getData } from "./data.js";
+import styles from "./table.module.css";
 console.log(getData());
 import "./App.css";
 
@@ -25,25 +26,108 @@ function App() {
   filtered.sort(sortAnimal);
 
   return (
-    <div>
-      <button onClick={() => setFilterProperty("")}>All animals</button>
-      <button onClick={() => setFilterProperty("dog")}>Dogs</button>
-      <button onClick={() => setFilterProperty("cat")}>Cats</button>
-
+    <div className={styles.table}>
+      <button className={styles.button} onClick={() => setFilterProperty("")}>
+        All animals
+      </button>
+      <button
+        className={styles.button}
+        onClick={() => setFilterProperty("dog")}
+      >
+        Dogs
+      </button>
+      <button
+        className={styles.button}
+        onClick={() => setFilterProperty("cat")}
+      >
+        Cats
+      </button>
       <table>
         <thead>
           <tr>
             <th>
-              <button onClick={() => setSortColumn("name")}>Name</button>
+              <button
+                className={styles.buttonSort}
+                onClick={() => setSortColumn("name")}
+              >
+                Name
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-arrow-down-short"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4z"
+                  />
+                </svg>
+              </button>
             </th>
             <th>
-              <button onClick={() => setSortColumn("trait")}>Trait</button>
+              <button
+                className={styles.buttonSort}
+                onClick={() => setSortColumn("trait")}
+              >
+                Trait
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-arrow-down-short"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4z"
+                  />
+                </svg>
+              </button>
             </th>
             <th>
-              <button onClick={() => setSortColumn("species")}>Species</button>
+              <button
+                className={styles.buttonSort}
+                onClick={() => setSortColumn("species")}
+              >
+                Species
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-arrow-down-short"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4z"
+                  />
+                </svg>
+              </button>
             </th>
             <th>
-              <button onClick={() => setSortColumn("age")}>Age</button>
+              <button
+                className={styles.buttonSort}
+                onClick={() => setSortColumn("age")}
+              >
+                Age
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-arrow-down-short"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4z"
+                  />
+                </svg>
+              </button>
             </th>
           </tr>
         </thead>
